a2a0950d9c79785b101df7059ee65874728b0acf
djones1117 <socjones21@yahoo.com>
2023-07-21 09:16:34 -0400

    renders scores and date

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,29 +1,21 @@
+import React from "react";
+import Score from "./Score";
 
 export default function Student({ student }) {
   return (
     <li className={"Student"}>
       <h3>{student.name}</h3>
       <p>{student.bio}</p>
+      <h4>Scores:</h4>
+      <ul>
+        {student.scores.map((score, index) => (
+          <li key={index}>
+            <Score score={score} />
+          </li>
+        ))}
+      </ul>
     </li>
   );
 }
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-      {/* <p>
-        Scores:{" "}
-        {student.scores.map((score) => (
-          <span key={score.date}>{score.score}, </span>
-        ))}
-      </p>
-    </li>
-  );
-}  */}
+
+
